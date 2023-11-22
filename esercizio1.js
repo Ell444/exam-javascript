db55b6fead137fe78a5daec79865ed3e0cb641fa
Laura Sirigu <laurasirigu43@gmail.com>
2023-11-22 10:33:04 +0100

    Feat || Primo esercizio

---
+++ esercizio1.js
@@ -0,0 +1,10 @@
+/*Esercizio 1 - Variabili e stringhe*/
+
+const userName = prompt(`What's your name, user?`);
+const userYearOfBirth = prompt(`Tell me your year of birth`);
+
+if(userYearOfBirth % 4 === 0 && userYearOfBirth % 100 !== 0){
+    alert(`Hi ${userName}, you were born in a leap year!`)
+}else{
+    alert(`Hi ${userName}, you were not born in a leap year!`)
+};
