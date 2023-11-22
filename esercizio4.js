3decb7a30b90a2e111cdc86580801f61a2b4f3fa
Laura Sirigu <laurasirigu43@gmail.com>
2023-11-22 14:41:00 +0100

    refact || es4, commentato cosa non richiesta.

--- esercizio4.js
+++ esercizio4.js
@@ -17,7 +17,7 @@ window.addEventListener('load', () => {
         const li = document.createElement('li');
         li.innerHTML = input.value;
         ul.appendChild(li);
-        input.value = '';
+        //input.value = ''; -- Per svuotare l'input all'invio.
        }
 
      })
